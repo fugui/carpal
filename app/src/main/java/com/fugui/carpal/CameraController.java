3b9d8d26970d9296392253d19558972bd68dd9a6
Fu Gui <szfugui@gmail.com>
2025-10-23 09:45:08 +0800

    使用 Google MLKit Text Recognition 进行文本识别（效果比 PaddleOCR 差许多）

--- app/src/main/java/com/fugui/carpal/CameraController.java
+++ app/src/main/java/com/fugui/carpal/CameraController.java
@@ -20,13 +20,25 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.chinese.ChineseTextRecognizerOptions;
+
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 public class CameraController {
 
@@ -38,7 +50,7 @@ public class CameraController {
     private final DetectionCallback detectionCallback;
     private ExecutorService cameraExecutor;
 
-    private static  Bitmap debugPicture;
+    private static Bitmap debugPicture;
 
     public CameraController(Context context, LifecycleOwner lifecycleOwner, PreviewView previewView, VehicleDetector vehicleDetector, DetectionCallback detectionCallback) {
         this.context = context;
@@ -48,7 +60,7 @@ public class CameraController {
         this.detectionCallback = detectionCallback;
 
 
-        try (InputStream inputStream = context.getAssets().open("road.jpeg")) {
+        try (InputStream inputStream = context.getAssets().open("road1.jpeg")) {
             debugPicture = BitmapFactory.decodeStream(inputStream);
         } catch (Exception e) {
             debugPicture = null;
@@ -101,15 +113,61 @@ public class CameraController {
             if (currentTime - lastAnalyzedTimestamp.get() >= ANALYSIS_INTERVAL_MS) {
                 lastAnalyzedTimestamp.set(currentTime);
 
-                Bitmap bitmap =  debugPicture; // imageProxy.toBitmap();
+                Bitmap bitmap = imageProxy.toBitmap(); // debugImage
                 if (bitmap != null) {
 
                     List<DetectionResult> detections = vehicleDetector.detect(bitmap);
-                    Bitmap imageWithDetections = drawDetections(bitmap, detections);
-                    detectionCallback.onDetections(imageWithDetections);
+                    textRecognizeDetections(bitmap, detections);
+
                 }
             }
             imageProxy.close();
+        }
+
+        private void textRecognizeDetections(Bitmap bitmap, List<DetectionResult> detections) {
+
+            TextRecognizer recognizer = TextRecognition.getClient(new ChineseTextRecognizerOptions.Builder().build());
+
+
+            List<Task<Text>> listTasks = detections.stream().map(detection -> {
+                        RectF rectF = detection.getBoundingBox();
+
+                        InputImage image = InputImage.fromBitmap(
+                                Bitmap.createBitmap(bitmap, (int) rectF.left, (int) rectF.top, (int) rectF.width(), (int) rectF.height()),
+                                0);
+                        return recognizer.process(image);
+                    }
+            ).collect(Collectors.toList());
+
+            Tasks.whenAllComplete(listTasks)
+                    .addOnCompleteListener(result -> {
+                        if (!result.isSuccessful())
+                            return;
+                        List<Task<?>> completedTasks = result.getResult();
+
+                        for (int i = 0; i < completedTasks.size(); i++) {
+                            Text visionText = (Text) completedTasks.get(i).getResult();
+                            String resultText = visionText.getText();
+                            Log.i(TAG, "Recognized Text: " + resultText);
+//                            for (Text.TextBlock block : visionText.getTextBlocks()) {
+//                                String blockText = block.getText();
+//                                Log.i(TAG, "TextBlock: " + blockText);
+//                                for (Text.Line line : block.getLines()) {
+//                                    String lineText = line.getText();
+//                                    Log.i(TAG, "  Line: " + lineText);
+//                                    for (Text.Element element : line.getElements()) {
+//                                        String elementText = element.getText();
+//                                        Log.i(TAG, "    Element: " + elementText);
+//                                    }
+//                                }
+//                            }
+                            detections.get(i).setText( String.join( " ", resultText.split("\n")));
+                        }
+
+
+                        detectionCallback.onDetections( drawDetections( bitmap, detections  ));
+                    })
+                    .addOnFailureListener(e -> Log.e(TAG, "Text recognition failed.", e));
         }
 
         private Bitmap drawDetections(Bitmap bitmap, List<DetectionResult> detections) {
@@ -118,17 +176,19 @@ public class CameraController {
             Paint paint = new Paint();
             paint.setColor(Color.RED);
             paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(4.0f);
-            paint.setTextSize(40.0f);
+            paint.setStrokeWidth(2.0f);
+            paint.setTextSize(30.0f);
 
             for (DetectionResult detection : detections) {
                 canvas.drawRect(detection.getBoundingBox(), paint);
                 String label = detection.getClassName() + ": " + String.format("%.2f", detection.getConfidence());
                 canvas.drawText(label, detection.getBoundingBox().left, detection.getBoundingBox().top - 10, paint);
+                canvas.drawText(detection.getText(), detection.getBoundingBox().left, detection.getBoundingBox().top + 25, paint);
             }
             return mutableBitmap;
         }
     }
+
 
     public void stopCamera() {
         if (cameraExecutor != null) {
--- app/src/main/java/com/fugui/carpal/DetectionResult.java
+++ app/src/main/java/com/fugui/carpal/DetectionResult.java
@@ -8,6 +8,8 @@ public class DetectionResult {
     private int classId;
     private String className;
 
+    private String text;
+
     public DetectionResult(RectF boundingBox, float confidence, int classId, String className) {
         this.boundingBox = boundingBox;
         this.confidence = confidence;
@@ -20,4 +22,12 @@ public class DetectionResult {
     public float getConfidence() { return confidence; }
     public int getClassId() { return classId; }
     public String getClassName() { return className; }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
